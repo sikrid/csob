98d72a5d98cf32625c27ae585939a747815cd486
davidwojnar67 <84183985+davidwojnar67@users.noreply.github.com>
2023-10-12 13:29:16 +0200

    Add Client

---
+++ src/CsobNG/ClientAPI/Data/ClientDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace ClientAPI.Data
+{
+    public class ClientDbContext : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+        public ClientDbContext(DbContextOptions<ClientDbContext> options) : base(options)
+
+        {
+        }
+    }
+}
--- src/CsobNG/ClientAPI/Program.cs
+++ src/CsobNG/ClientAPI/Program.cs
@@ -1,11 +1,19 @@
+using ClientAPI.Data;
+using ClientAPI.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ClientDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ClientService>();
 
 var app = builder.Build();
 
