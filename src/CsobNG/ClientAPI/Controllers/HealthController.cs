daa0f0be0606a36ec86f36c318fc69accc6bdd62
Matěj Kotlín <matej.kotlin@gmail.com>
2023-10-12 10:54:28 +0200

    health status

---
+++ src/CsobNG/ClientAPI/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("")]
+        public ActionResult<string> GetStatus()
+        {
+            return Ok("healthy");
+        }
+    }
+}
