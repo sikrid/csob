c2be10a22b8f7e71209507aa560fd52d2b423eab
Petr Kolář <kolar.ptr@gmail.com>
2023-10-12 12:39:09 +0200

    endpoints

---
+++ src/CsobNG/Admin/Data/Endpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace Admin.Data
+{
+    public class Endpoints
+    {
+        /// <summary>
+        /// NazevAPI, URL
+        /// </summary>
+        public static Dictionary<string, string> HealthEndpoints = new()
+        {
+            {"ClientAPI", "http://localhost:5208" },
+			{"LoanAPI", "http://localhost:5174" },
+			{"EmailsAPI", "http://localhost:5032" },
+			{"ClientBFF", "http://localhost:5025" }
+		};
+    }
+}
