55ac8bebfcbec91d8a50c03c24b4207e7e819256
Matěj Kotlín <matej.kotlin@gmail.com>
2023-10-12 15:40:47 +0200

    ClientAPI cors update

--- src/CsobNG/ClientAPI/Program.cs
+++ src/CsobNG/ClientAPI/Program.cs
@@ -24,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseAuthorization();
 
 app.MapControllers();
