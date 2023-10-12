08c52e0d1c4522e3fdd07ef32b8d7f7a813ebf72
ftichy <59052141+ftichy@users.noreply.github.com>
2023-10-12 11:01:04 +0200

    Add HealthController

---
+++ src/CsobNG/EmailsAPI/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmailsAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("")]
+
+        public ActionResult<string> GetStatus()
+        {
+            return Ok("healthy");
+        }
+    }
+}
