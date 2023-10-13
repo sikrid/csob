77b7248d1d29e17652e79dcd42f12e936df25323
kubicek-skoleni <kubicek.brno@gmail.com>
2023-10-13 13:18:40 +0200

    dockerized dclient api

--- src/CsobNG/ClientAPI/Program.cs
+++ src/CsobNG/ClientAPI/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DockerDesktop") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ClientDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
